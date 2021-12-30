645657e94190b04f3fa9a7361176700ef2628c66
Aniket1118 <91197235+Aniket1118@users.noreply.github.com>
2021-12-30 18:07:50 +0530

    first commit

---
+++ class.js
@@ -0,0 +1,59 @@
+// ASSIGNMENT 1 Factorial of a number if input is 5
+//take n from the user and print the factorial 
+/*
+var num = 5;
+var fact = 1;
+for (i = 1; i <= num; i++) {
+    fact =fact*i;
+
+}
+console.log(fact)
+*/
+/* FACTORIAL OF 7
+
+var num = 7;
+var fact = 1;
+for (i = 1; i <= num; i++) {
+    fact =fact*i;
+
+}
+console.log(fact)
+*/
+
+//take n from the user and print the factorial 
+/*
+var num = parseInt(prompt("enter the number"));
+var fact = 1;
+for (i = 1; i <= num; i++) {
+    fact =fact*i;
+
+}
+console.log(fact)
+*/
+
+
+//ASSIGNMENT 2
+// assignment -> take n from user and print first n prime numbers
+/*
+var num = parseInt(prompt("enter the number "));
+for(i=2;i<num;i++){
+    for(j=2;j<i;j++) {
+        if(i%j == 0){
+            console.log(i)
+        break;}}
+        
+}
+
+*/
+//ASSIGNMENT 3 -> check if a num is a perfect square or not
+
+/*
+var num = prompt("enter the number you want to check ");
+num = parseInt(num);
+for(i=2;i<num;i++) {
+    if((num%i == 0) && ((i*i) == num)){
+        alert("this is a perfect square");
+    }
+   
+else{ 
+    alert("this is not");}} */
